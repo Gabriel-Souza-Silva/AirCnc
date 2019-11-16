28f1fa0bd0ba21f2d64562c548ada585fe872884
Gabriel Souza <gabrielccpuc@gmail.com>
2019-11-16 08:07:15 -0300

    alterando cors

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@
 const express = require ('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
+const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
@@ -15,7 +17,8 @@ mongoose.connect('mongodb+srv://OiMeuChapa:Testeteste@cluster0-f5gl2.mongodb.net
     useUnifiedTopology:true
 })
 
-
+app.use(cors())
 app.use(express.json())
+app.use('/files',express.static(path.resolve(__dirname,'..','uploads')))
 app.use(routes)
 app.listen(3333)
